da4bd6d32157b5ea1a7d0824c89911126569cfb7
Özge Ahras <ozgeahras@gmail.com>
2023-05-16 10:59:00 +0300

    updated configuration and added synopsis fn

--- index.js
+++ index.js
@@ -1,25 +1,61 @@
-const setupTextarea = document.getElementById("setup-textarea");
+import { process } from "/env";
+import { Configuration, OpenAIApi } from "openai";
+
 const setupInputContainer = document.getElementById("setup-input-container");
 const movieBossText = document.getElementById("movie-boss-text");
 
-const apiKey = "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";
-const url = "https://api.openai.com/v1/completions";
-
-document.getElementById("send-btn").addEventListener("click", () => {
-  // if (setupTextarea.value) {
-  setupInputContainer.innerHTML = `<img src="images/loading.svg" class="loading" id="loading">`;
-  movieBossText.innerText = `Ok, just wait a second while my digital brain digests that...`;
-  // }
-  fetchBotReply();
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
 });
 
-function fetchBotReply() {
-  /*
-Challenge:
-  1. Make a fetch request to OpenAi API. 
-  2. The prompt should request an enthusiastic response
-     in no more than 5 words. 
-  3. For now you can just log out the completion to check 
-     it's working. 
-*/
+const openai = new OpenAIApi(configuration);
+
+document.getElementById("send-btn").addEventListener("click", () => {
+  const setupTextarea = document.getElementById("setup-textarea");
+  if (setupTextarea.value) {
+    const userInput = setupTextarea.value;
+    setupInputContainer.innerHTML = `<img src="images/loading.svg" class="loading" id="loading">`;
+    movieBossText.innerText = `Ok, just wait a second while my digital brain digests that...`;
+    fetchBotReply(userInput);
+  }
+});
+
+async function fetchBotReply(outline) {
+  const response = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: `Generate a short message to enthusiastically say an outline sounds interesting and that you need some minutes to think about it.
+    ###
+    outline: Two dogs fall in love and move to Hawaii to learn to surf.
+    message: I'll need to think about that. But your idea is amazing! I love the bit about Hawaii!
+    ###
+    outline: A plane crashes in the jungle and the passengers have to walk 1000km to safety.
+    message: I'll spend a few moments considering that. But I love your idea!! A disaster movie in the jungle!
+    ###
+    outline: A group of corrupt lawyers try to send an innocent woman to jail.
+    message: Wow that is awesome! Corrupt lawyers, huh? Give me a few moments to think!
+    ###
+    outline: ${outline}
+    message: 
+    `,
+    max_tokens: 60,
+  });
+  movieBossText.innerText = response.data.choices[0].text.trim();
+  console.log(response);
+}
+
+async function fetchSynopsis(outline) {
+  const response = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: `Generate an engaging, professional and marketable movie synopsis based on an outline
+    ###
+    outline: A big-headed daredevil fighter pilot goes back to school only to be sent on a deadly mission.
+    synopsis: The Top Gun Naval Fighter Weapons School is where the best of the best train to refine their elite flying skills. When hotshot fighter pilot Maverick (Tom Cruise) is sent to the school, his reckless attitude and cocky demeanor put him at odds with the other pilots, especially the cool and collected Iceman (Val Kilmer). But Maverick isn't only competing to be the top fighter pilot, he's also fighting for the attention of his beautiful flight instructor, Charlotte Blackwood (Kelly McGillis). Maverick gradually earns the respect of his instructors and peers - and also the love of Charlotte, but struggles to balance his personal and professional life. As the pilots prepare for a mission against a foreign enemy, Maverick must confront his own demons and overcome the tragedies rooted deep in his past to become the best fighter pilot and return from the mission triumphant.
+    ###
+    outline: ${outline}
+    synopsis: 
+    `,
+    max_tokens: 700,
+  });
+  document.getElementById("output-text").innerText =
+    response.data.choices[0].text.trim();
 }
