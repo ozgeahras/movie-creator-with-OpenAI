7dbbd92f99317c9824605d15d411183c50304c5b
Özge Ahras <ozge.ahras@ykteknoloji.com.tr>
2023-06-13 12:57:22 +0300

    added openaiconfig for all function files and make some code review

--- netlify/functions/fetchSynopsis.js
+++ netlify/functions/fetchSynopsis.js
@@ -1,13 +1,4 @@
-const { Configuration, OpenAIApi } = require("openai");
-
-const apiKey = process.env.VITE_MOVIE_APP_API_KEY;
-
-const configuration = new Configuration({
-  organization: "org-8sBBKnnzpa0m1QQdIgYJtvhS",
-  apiKey: apiKey,
-});
-delete configuration.baseOptions.headers["User-Agent"];
-const openai = new OpenAIApi(configuration);
+const openai = require("../openaiConfig.js");
 
 exports.handler = async (event) => {
   try {
