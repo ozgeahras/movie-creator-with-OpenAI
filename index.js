667714197b6ce964dc8027038aaaa0e19ae62ee5
Özge Ahras <ozge.ahras@ykteknoloji.com.tr>
2023-06-13 12:01:58 +0300

    updated netlify functions

--- index.js
+++ index.js
@@ -1,14 +1,3 @@
-import { Configuration, OpenAIApi } from "openai";
-
-const apiKey = import.meta.env.VITE_MOVIE_APP_API_KEY;
-
-const configuration = new Configuration({
-  organization: "org-8sBBKnnzpa0m1QQdIgYJtvhS",
-  apiKey: apiKey,
-});
-delete configuration.baseOptions.headers["User-Agent"];
-const openai = new OpenAIApi(configuration);
-
 const setupInputContainer = document.getElementById("setup-input-container");
 const movieBossText = document.getElementById("movie-boss-text");
 
@@ -24,113 +13,74 @@ document.getElementById("send-btn").addEventListener("click", () => {
 });
 
 async function fetchBotReply(outline) {
-  const response = await openai.createCompletion({
-    model: "text-davinci-003",
-    prompt: `Generate a short message to enthusiastically say an outline sounds interesting and that you need some minutes to think about it.
-    ###
-    outline: Two dogs fall in love and move to Hawaii to learn to surf.
-    message: I'll need to think about that. But your idea is amazing! I love the bit about Hawaii!
-    ###
-    outline:A plane crashes in the jungle and the passengers have to walk 1000km to safety.
-    message: I'll spend a few moments considering that. But I love your idea!! A disaster movie in the jungle!
-    ###
-    outline: A group of corrupt lawyers try to send an innocent woman to jail.
-    message: Wow that is awesome! Corrupt lawyers, huh? Give me a few moments to think!
-    ###
-    outline: ${outline}
-    message: 
-    `,
-    max_tokens: 60,
+  const response = await fetch("/.netlify/functions/fetchBotReply", {
+    method: "POST",
+    body: JSON.stringify({ outline }),
+    headers: { "Content-Type": "application/json" },
   });
-  movieBossText.innerText = response.data.choices[0].text.trim();
+  const data = await response.json();
+  movieBossText.innerText = data.text.trim();
 }
 
 async function fetchSynopsis(outline) {
-  const response = await openai.createCompletion({
-    model: "text-davinci-003",
-    prompt: `Generate an engaging, professional and marketable movie synopsis based on an outline. The synopsis should include actors names in brackets after each character. Choose actors that would be ideal for this role. 
-    ###
-    outline: A big-headed daredevil fighter pilot goes back to school only to be sent on a deadly mission.
-    synopsis: The Top Gun Naval Fighter Weapons School is where the best of the best train to refine their elite flying skills. When hotshot fighter pilot Maverick (Tom Cruise) is sent to the school, his reckless attitude and cocky demeanor put him at odds with the other pilots, especially the cool and collected Iceman (Val Kilmer). But Maverick isn't only competing to be the top fighter pilot, he's also fighting for the attention of his beautiful flight instructor, Charlotte Blackwood (Kelly McGillis). Maverick gradually earns the respect of his instructors and peers - and also the love of Charlotte, but struggles to balance his personal and professional life. As the pilots prepare for a mission against a foreign enemy, Maverick must confront his own demons and overcome the tragedies rooted deep in his past to become the best fighter pilot and return from the mission triumphant.  
-    ###
-    outline: ${outline}
-    synopsis: 
-    `,
-    max_tokens: 700,
+  const response = await fetch("/.netlify/functions/fetchSynopsis", {
+    method: "POST",
+    body: JSON.stringify({ outline }),
+    headers: { "Content-Type": "application/json" },
   });
-  const synopsis = response.data.choices[0].text.trim();
+  const data = await response.json();
+  const synopsis = data.text.trim();
   document.getElementById("output-text").innerText = synopsis;
   fetchTitle(synopsis);
   fetchStars(synopsis);
 }
 
 async function fetchTitle(synopsis) {
-  const response = await openai.createCompletion({
-    model: "text-davinci-003",
-    prompt: `Generate a catchy movie title for this synopsis: ${synopsis}`,
-    max_tokens: 25,
-    temperature: 0.7,
+  const response = await fetch("/.netlify/functions/fetchTitle", {
+    method: "POST",
+    body: JSON.stringify({ synopsis }),
+    headers: { "Content-Type": "application/json" },
   });
-  const title = response.data.choices[0].text.trim();
+  const data = await response.json();
+  const title = data.text.trim();
   document.getElementById("output-title").innerText = title;
-  fetchImagePromt(title, synopsis);
+  fetchImagePrompt(title, synopsis);
 }
 
 async function fetchStars(synopsis) {
-  const response = await openai.createCompletion({
-    model: "text-davinci-003",
-    prompt: `Extract the names in brackets from the synopsis.
-    ###
-    synopsis: The Top Gun Naval Fighter Weapons School is where the best of the best train to refine their elite flying skills. When hotshot fighter pilot Maverick (Tom Cruise) is sent to the school, his reckless attitude and cocky demeanor put him at odds with the other pilots, especially the cool and collected Iceman (Val Kilmer). But Maverick isn't only competing to be the top fighter pilot, he's also fighting for the attention of his beautiful flight instructor, Charlotte Blackwood (Kelly McGillis). Maverick gradually earns the respect of his instructors and peers - and also the love of Charlotte, but struggles to balance his personal and professional life. As the pilots prepare for a mission against a foreign enemy, Maverick must confront his own demons and overcome the tragedies rooted deep in his past to become the best fighter pilot and return from the mission triumphant.
-    names: Tom Cruise, Val Kilmer, Kelly McGillis
-    ###
-    synopsis: ${synopsis}
-    names:   
-    `,
-    max_tokens: 30,
+  const response = await fetch("/.netlify/functions/fetchStars", {
+    method: "POST",
+    body: JSON.stringify({ synopsis }),
+    headers: { "Content-Type": "application/json" },
   });
-  document.getElementById("output-stars").innerText =
-    response.data.choices[0].text.trim();
+  const data = await response.json();
+  document.getElementById("output-stars").innerText = data.text.trim();
 }
 
-async function fetchImagePromt(title, synopsis) {
-  const response = await openai.createCompletion({
-    model: "text-davinci-003",
-    prompt: `Give a short description of an image which could be used to advertise a movie based on a title and synopsis. The description should be rich in visual detail but contain no names.
-    ###
-    title: Love's Time Warp
-    synopsis: When scientist and time traveller Wendy (Emma Watson) is sent back to the 1920s to assassinate a future dictator, she never expected to fall in love with them. As Wendy infiltrates the dictator's inner circle, she soon finds herself torn between her mission and her growing feelings for the leader (Brie Larson). With the help of a mysterious stranger from the future (Josh Brolin), Wendy must decide whether to carry out her mission or follow her heart. But the choices she makes in the 1920s will have far-reaching consequences that reverberate through the ages.
-    image description: A silhouetted figure stands in the shadows of a 1920s speakeasy, her face turned away from the camera. In the background, two people are dancing in the dim light, one wearing a flapper-style dress and the other wearing a dapper suit. A semi-transparent image of war is super-imposed over the scene.
-    ###
-    title: zero Earth
-    synopsis: When bodyguard Kob (Daniel Radcliffe) is recruited by the United Nations to save planet Earth from the sinister Simm (John Malkovich), an alien lord with a plan to take over the world, he reluctantly accepts the challenge. With the help of his loyal sidekick, a brave and resourceful hamster named Gizmo (Gaten Matarazzo), Kob embarks on a perilous mission to destroy Simm. Along the way, he discovers a newfound courage and strength as he battles Simm's merciless forces. With the fate of the world in his hands, Kob must find a way to defeat the alien lord and save the planet.
-    image description: A tired and bloodied bodyguard and hamster standing atop a tall skyscraper, looking out over a vibrant cityscape, with a rainbow in the sky above them.
-    ###
-    title: ${title}
-    synopsis: ${synopsis}
-    image description: 
-    `,
-    temperature: 0.8,
-    max_tokens: 100,
+async function fetchImagePrompt(title, synopsis) {
+  const response = await fetch("/.netlify/functions/fetchImagePrompt", {
+    method: "POST",
+    body: JSON.stringify({ title, synopsis }),
+    headers: { "Content-Type": "application/json" },
   });
-  fetchImageUrl(response.data.choices[0].text.trim());
+  const data = await response.json();
+  fetchImageUrl(data.text.trim());
 }
 
 async function fetchImageUrl(imagePrompt) {
-  const response = await openai.createImage({
-    prompt: `${imagePrompt}. There should be no text in this image.`,
-    n: 1,
-    size: "256x256",
-    response_format: "b64_json",
+  const response = await fetch("/.netlify/functions/fetchImageUrl", {
+    method: "POST",
+    body: JSON.stringify({ imagePrompt }),
+    headers: { "Content-Type": "application/json" },
   });
-
+  const data = await response.json();
   document.getElementById(
     "output-img-container"
-  ).innerHTML = `<img src="data:image/png;base64,${response.data.data[0].b64_json}">`;
+  ).innerHTML = `<img src="${data.imageUrl}">`;
   setupInputContainer.innerHTML = `<button id="view-pitch-btn" class="view-pitch-btn">View Pitch</button>`;
   document.getElementById("view-pitch-btn").addEventListener("click", () => {
-    document.getElementById("setup - container").style.display = "none";
-    document.getElementById("output - container").style.display = "flex";
+    document.getElementById("setup-container").style.display = "none";
+    document.getElementById("output-container").style.display = "flex";
     movieBossText.innerText = `This idea is so good I'm jealous! It's gonna make you rich for sure! Remember, I want 10% 💰`;
   });
 }
--- netlify/functions/fetchOpenAI/fetchOpenAI.js
+++ netlify/functions/fetchOpenAI/fetchOpenAI.js
@@ -1,17 +1,177 @@
-// Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
-const handler = async (event) => {
+const { Configuration, OpenAIApi } = require("openai");
+
+const apiKey = process.env.VITE_MOVIE_APP_API_KEY;
+
+const configuration = new Configuration({
+  organization: "org-8sBBKnnzpa0m1QQdIgYJtvhS",
+  apiKey: apiKey,
+});
+delete configuration.baseOptions.headers["User-Agent"];
+const openai = new OpenAIApi(configuration);
+
+exports.handler = async (event, context) => {
   try {
-    const subject = event.queryStringParameters.name || 'World'
-    return {
+    const { body } = event;
+    const { userInput } = JSON.parse(body);
+
+    const botReply = await fetchBotReply(userInput);
+    const synopsis = await fetchSynopsis(userInput);
+    const title = await fetchTitle(synopsis);
+    const stars = await fetchStars(synopsis);
+    const imageUrl = await fetchImagePrompt(title, synopsis);
+
+    const response = {
       statusCode: 200,
-      body: JSON.stringify({ message: `Hello ${subject}` }),
-      // // more keys you can return:
-      // headers: { "headerName": "headerValue", ... },
-      // isBase64Encoded: true,
-    }
+      body: JSON.stringify({
+        botReply,
+        synopsis,
+        title,
+        stars,
+        imageUrl,
+      }),
+    };
+
+    return response;
   } catch (error) {
-    return { statusCode: 500, body: error.toString() }
+    console.error(error);
+
+    const response = {
+      statusCode: 500,
+      body: JSON.stringify({ error: "An error occurred" }),
+    };
+
+    return response;
   }
+};
+
+async function fetchBotReply(outline) {
+  const response = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: `Generate a short message to enthusiastically say an outline sounds interesting and that you need some minutes to think about it.
+    ###
+    outline: Two dogs fall in love and move to Hawaii to learn to surf.
+    message: I'll need to think about that. But your idea is amazing! I love the bit about Hawaii!
+    ###
+    outline:A plane crashes in the jungle and the passengers have to walk 1000km to safety.
+    message: I'll spend a few moments considering that. But I love your idea!! A disaster movie in the jungle!
+    ###
+    outline: A group of corrupt lawyers try to send an innocent woman to jail.
+    message: Wow that is awesome! Corrupt lawyers, huh? Give me a few moments to think!
+    ###
+    outline: ${outline}
+    message: 
+    `,
+    max_tokens: 60,
+  });
+  return response.data.choices[0].text.trim();
 }
 
-module.exports = { handler }
+async function fetchSynopsis(outline) {
+  const response = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: `Generate an engaging, professional and marketable movie synopsis based on an outline. The synopsis should include actors names in brackets after each character. Choose actors that would be ideal for this role. 
+    ###
+    outline: A big-headed daredevil fighter pilot goes back to school only to be sent on a deadly mission.
+    synopsis: The Top Gun Naval Fighter Weapons School is where the best of the best train to refine their elite flying skills. When hotshot fighter pilot Maverick (Tom Cruise) is sent to the school, his reckless attitude and cocky demeanor put him at odds with the other pilots, especially the cool and collected Iceman (Val Kilmer). But Maverick isn't only competing to be the top fighter pilot, he's also fighting for the attention of his beautiful flight instructor, Charlotte Blackwood (Kelly McGillis). Maverick gradually earns the respect of his instructors and peers - and also the love of Charlotte, but struggles to balance his personal and professional life. As the pilots prepare for a mission against a foreign enemy, Maverick must confront his own demons and overcome the tragedies rooted deep in his past to become the best fighter pilot and return from the mission triumphant.  
+    ###
+    outline: ${outline}
+    synopsis: 
+    `,
+    max_tokens: 700,
+  });
+  return response.data.choices[0].text.trim();
+}
+
+async function fetchTitle(synopsis) {
+  const response = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: `Generate a catchy movie title for this synopsis: ${synopsis}`,
+    max_tokens: 25,
+    temperature: 0.7,
+  });
+  return response.data.choices[0].text.trim();
+}
+
+async function fetchStars(synopsis) {
+  const response = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: `Extract the names in brackets from the synopsis.
+    ###
+    synopsis: The Top Gun Naval Fighter Weapons School is where the best of the best train to refine their elite flying skills. When hotshot fighter pilot Maverick (Tom Cruise) is sent to the school, his reckless attitude and cocky demeanor put him at odds with the other pilots, especially the cool and collected Iceman (Val Kilmer). But Maverick isn't only competing to be the top fighter pilot, he's also fighting for the attention of his beautiful flight instructor, Charlotte Blackwood (Kelly McGillis). Maverick gradually earns the respect of his instructors and peers - and also the love of Charlotte, but struggles to balance his personal and professional life. As the pilots prepare for a mission against a foreign enemy, Maverick must confront his own demons and overcome the tragedies rooted deep in his past to become the best fighter pilot and return from the mission triumphant.
+    names: Tom Cruise, Val Kilmer, Kelly McGillis
+    ###
+    synopsis: ${synopsis}
+    names:   
+    `,
+    max_tokens: 30,
+  });
+  return response.data.choices[0].text.trim();
+}
+
+async function fetchImagePrompt(title, synopsis) {
+  const response = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: `Give a short description of an image which could be used to advertise a movie based on a title and synopsis. The description should be rich in visual detail but contain no names.
+    ###
+    title: Love's Time Warp
+    synopsis: When scientist and time traveller Wendy (Emma Watson) is sent back to the 1920s to assassinate a future dictator, she never expected to fall in love with them. As Wendy infiltrates the dictator's inner circle, she soon finds herself torn between her mission and her growing feelings for the leader (Brie Larson). With the help of a mysterious stranger from the future (Josh Brolin), Wendy must decide whether to carry out her mission or follow her heart. But the choices she makes in the 1920s will have far-reaching consequences that reverberate through the ages.
+    image description: A silhouetted figure stands in the shadows of a 1920s speakeasy, her face turned away from the camera. In the background, two people are dancing in the dim light, one wearing a flapper-style dress and the other wearing a dapper suit. A semi-transparent image of war is super-imposed over the scene.
+    ###
+    title: zero Earth
+    synopsis: When bodyguard Kob (Daniel Radcliffe) is recruited by the United Nations to save planet Earth from the sinister Simm (John Malkovich), an alien lord with a plan to take over the world, he reluctantly accepts the challenge. With the help of his loyal sidekick, a brave and resourceful hamster named Gizmo (Gaten Matarazzo), Kob embarks on a perilous mission to destroy Simm. Along the way, he discovers a newfound courage and strength as he battles Simm's merciless forces. With the fate of the world in his hands, Kob must find a way to defeat the alien lord and save the planet.
+    image description: A tired and bloodied bodyguard and hamster standing atop a tall skyscraper, looking out over a vibrant cityscape, with a rainbow in the sky above them.
+    ###
+    title: ${title}
+    synopsis: ${synopsis}
+    image description: 
+    `,
+    temperature: 0.8,
+    max_tokens: 100,
+  });
+  return response.data.choices[0].text.trim();
+}
+
+async function fetchImageUrl(imagePrompt) {
+  const response = await openai.createImage({
+    prompt: `${imagePrompt}. There should be no text in this image.`,
+    n: 1,
+    size: "256x256",
+    response_format: "b64_json",
+  });
+
+  return response.data.data[0].b64_json;
+}
+
+exports.handler = async (event) => {
+  const apiKey = process.env.VITE_MOVIE_APP_API_KEY;
+
+  const configuration = new Configuration({
+    organization: "org-8sBBKnnzpa0m1QQdIgYJtvhS",
+    apiKey: apiKey,
+  });
+  delete configuration.baseOptions.headers["User-Agent"];
+  const openai = new OpenAIApi(configuration);
+
+  const body = JSON.parse(event.body);
+  const outline = body.outline;
+
+  const botReply = await fetchBotReply(outline);
+  const synopsis = await fetchSynopsis(outline);
+  const title = await fetchTitle(synopsis);
+  const stars = await fetchStars(synopsis);
+  const imagePrompt = await fetchImagePrompt(title, synopsis);
+  const imageUrl = await fetchImageUrl(imagePrompt);
+
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      botReply: botReply,
+      synopsis: synopsis,
+      title: title,
+      stars: stars,
+      imageUrl: imageUrl,
+    }),
+  };
+
+  return response;
+};
